c1d01022a9366d9268d0348cb420a67a4c98691e
GoldanDavidTeodor <goldan.david.teodor@gmail.com>
2025-10-09 14:50:04 +0300

    tema2

---
+++ Tema2/My project (1)/Assets/Target.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro; 
+
+public class Target : MonoBehaviour
+{
+    public int baseScore = 10;
+    public Transform throwOrigin;
+    public TMP_Text scoreText; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Throwable"))
+        {
+            float distance = Vector3.Distance(throwOrigin.position, other.transform.position);
+            int finalScore = Mathf.Max(0, Mathf.RoundToInt(baseScore - distance));
+
+            if (scoreText != null)
+            {
+                scoreText.text = "Scor: " + finalScore;
+            }
+
+        }
+    }
+}
